cf3cbb664a9fd1c018922bafb419d6904ef1969e
senaphim <senaphim@gmail.com>
2025-12-02 20:53:47 +0000

    Added python for day 2/part 2. This is mostly the same as part 1, due to misreading part one and writing an overly general solution

---
+++ day_2/pysrc/part_2.py
@@ -0,0 +1,62 @@
+import math
+import re
+
+
+def main():
+    data_string = ""
+    all_invalid_ids = []
+
+    with open("../data.txt", "r") as file:
+        data_string = file.read()
+
+    spans = data_string.split(",")
+    for span in spans:
+        start, stop = span.split("-")
+        all_ids = [str(x) for x in range(int(start), int(stop) + 1)]
+        invalid_ids = find_invalid(all_ids)
+        all_invalid_ids += invalid_ids
+
+    total = 0
+    for id in all_invalid_ids:
+        total += int(id)
+
+    print(total)
+
+
+def find_invalid(ids):
+    invalid_ids = []
+
+    for id in ids:
+        factor_list = factorise(len(id))
+        prefix_list = []
+        for factor in factor_list:
+            prefix_list.append(id[:factor])
+
+        if prefix_match(prefix_list, id):
+            invalid_ids.append(id)
+
+    return invalid_ids
+
+
+def factorise(num):
+    factor_list = []
+    for i in range(1, math.floor(num / 2) + 1):
+        if num % i == 0:
+            factor_list.append(i)
+
+    return factor_list
+
+
+def prefix_match(prefix_list, id):
+    for prefix in prefix_list:
+        match_count = len(re.findall(prefix, id))
+        if match_count == len(id) / len(prefix):
+            # print(f"{prefix=} {id=} {match_count=}, Yes")
+            return True
+
+    # print(f"{prefix=} {id=} {match_count=}, No")
+    return False
+
+
+if __name__ == "__main__":
+    main()
